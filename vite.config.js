dfc7713ca2384914d56bd2d07805f04173143a13
minhhuyzzz <huy868762@gmail.com>
2026-02-08 16:44:05 +0700

    fix: manually add tailwind vite plugin to package.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Dòng này cực kỳ quan trọng
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Phải có dòng này để kích hoạt Tailwind v4
+    tailwindcss(),
   ],
 })
